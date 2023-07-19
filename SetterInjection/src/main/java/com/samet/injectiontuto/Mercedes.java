c2423ca74e7ee041defc9f1a79bce3bc60c3f698
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-07-19 14:53:31 +0300

    Setter Injection
    Setter injection java spring boot

---
+++ SetterInjection/src/main/java/com/samet/injectiontuto/Mercedes.java
@@ -0,0 +1,11 @@
+package com.samet.injectiontuto;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mercedes implements  Car{
+    @Override
+    public String brand() {
+        return "Mercedes is  the name of brand!!!!!!!!!!!!!";
+    }
+}
