ed88bd9ed4315f7cf32f304331ea2640b86963d2
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-07-22 15:46:08 +0300

    Read methods

---
+++ Database Access with Hibernate JPA/CarSQL-Read/src/main/java/com/samet/carsql/dao/CarDAO.java
@@ -0,0 +1,16 @@
+package com.samet.carsql.dao;
+
+import com.samet.carsql.entity.Car;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+public interface CarDAO {
+
+    Car findById(Integer id);
+
+    List<Car> findAll();
+
+    List<Car> findByName(String name);
+}
