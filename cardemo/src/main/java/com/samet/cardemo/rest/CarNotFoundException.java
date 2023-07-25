e9579d5720ba298dcb100dd948a476ab1bc00695
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-07-25 21:07:46 +0300

    Update Car demo
    Added Exception Handler

---
+++ cardemo/src/main/java/com/samet/cardemo/rest/CarNotFoundException.java
@@ -0,0 +1,15 @@
+package com.samet.cardemo.rest;
+
+public class CarNotFoundException extends  RuntimeException{
+    public CarNotFoundException(String message) {
+        super(message);
+    }
+
+    public CarNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CarNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
--- cardemo/src/main/java/com/samet/cardemo/rest/CarRestController.java
+++ cardemo/src/main/java/com/samet/cardemo/rest/CarRestController.java
@@ -3,10 +3,9 @@ package com.samet.cardemo.rest;
 
 import com.samet.cardemo.entity.Car;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +35,40 @@ public class CarRestController {
     //path variablee
     @GetMapping("/cars/{carId}")
     public Car getWorker(@PathVariable int carId){
+
+        if((carId>=theCars.size()) || (carId<0)){
+            throw new CarNotFoundException("ID not found - "+ carId);
+        }
+
         return  theCars.get(carId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<CarErrorResponse> handleException(CarNotFoundException err){
+
+        //create car  error  response
+        CarErrorResponse error = new CarErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(err.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+
+        //return it
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+    }
+
+    //add another exceptionhandler to catch all type
+    @ExceptionHandler
+    public ResponseEntity<CarErrorResponse> handleException(Exception exception){
+
+        //create car error response
+        CarErrorResponse error = new CarErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exception.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+
     }
 }
